749f236127957fb9b900998e235658476f1e09ce
p7157568 <p7157568@gmail.com>
2016-02-18 07:28:52 +0700

    Initial commit

---
+++ app/src/main/java/dhbk/android/lab1/MainActivity.java
@@ -0,0 +1,24 @@
+package dhbk.android.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nameField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText) findViewById(R.id.nameField);
+    }
+
+    public void showName(View view) {
+        Toast.makeText(MainActivity.this, "Hello " + nameField.getText(), Toast.LENGTH_SHORT).show();
+    }
+}
